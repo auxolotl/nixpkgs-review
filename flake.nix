0388ebaa3039fe8c62d16aaefdb7e10ce9d2a249
isabel <isabel@isabelroses.com>
2024-05-02 20:42:12 +0100

    chore: use auxolotl for nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nixpkgs-review";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:auxolotl/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
@@ -10,27 +10,40 @@
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs @ { flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } ({ lib, ... }: {
-      imports = [ ./treefmt.nix ];
-      systems = [
-        "aarch64-linux"
-        "x86_64-linux"
-        "riscv64-linux"
-
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      perSystem = { config, pkgs, self', ... }: {
-        packages = {
-          nixpkgs-review = pkgs.callPackage ./. { };
-          default = config.packages.nixpkgs-review;
-        } // lib.optionalAttrs (pkgs.stdenv.isLinux) {
-          nixpkgs-review-sandbox = pkgs.callPackage ./. { withSandboxSupport = true; };
-        };
-        devShells = {
-          default = (self'.packages.nixpkgs-review-sandbox or self'.packages.nixpkgs-review).override { withNom = true; };
-        };
-      };
-    });
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        imports = [ ./treefmt.nix ];
+        systems = [
+          "aarch64-linux"
+          "x86_64-linux"
+          "riscv64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ];
+        perSystem =
+          { config
+          , pkgs
+          , self'
+          , ...
+          }:
+          {
+            packages =
+              {
+                nixpkgs-review = pkgs.callPackage ./. { };
+                default = config.packages.nixpkgs-review;
+              }
+              // lib.optionalAttrs (pkgs.stdenv.isLinux) {
+                nixpkgs-review-sandbox = pkgs.callPackage ./. { withSandboxSupport = true; };
+              };
+            devShells = {
+              default = (self'.packages.nixpkgs-review-sandbox or self'.packages.nixpkgs-review).override {
+                withNom = true;
+              };
+            };
+          };
+      }
+    );
 }
